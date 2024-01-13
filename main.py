f2a4b542efd43f694591369804be4a22d9b217d7
PurviSaiChoure <purvichoure2@gmail.com>
2024-01-13 20:05:12 +0530

    model evaluation using mlflo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.Stage_01_data_inegstion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.Stage_02_prepare_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.Stage_03_model_trainer import ModelTrainingPipeline
+from cnnClassifier.pipeline.Stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = 'data ingestion stage'
 
@@ -33,6 +34,18 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,8 +3,10 @@ from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig
-                                                )
+                                                TrainingConfig,
+                                                EvaluationConfig)
+
+
 class ConfigurationManager:
     def __init__(
         self,
@@ -51,15 +53,7 @@ class ConfigurationManager:
 
         return prepare_base_model_config
     
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH):
 
-        self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filepath)
-
-        create_directories([self.config.artifacts_root])
 
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
@@ -80,4 +74,32 @@ class ConfigurationManager:
             params_is_augmentation=params.AUGMENTATION,
             params_image_size=params.IMAGE_SIZE
         )
+
         return training_config
+    
+
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/PurviSaiChoure/Chest-Cancer-Classification-using-DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+
+      
+
+
+
+
+
+
+
+
+
+
+
